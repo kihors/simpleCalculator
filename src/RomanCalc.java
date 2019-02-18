4c6c5fcbfdd000921d31e9e6db99cfab4c6efeba
kihors <kihors@gmail.com>
2019-02-18 03:03:17 +0200

    Created class RomanCalc

---
+++ src/RomanCalc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class RomanCalc {
+
+    private Operations operations = new Operations();
+    private Scanner scanner = new Scanner(System.in);
+
+    void calculator() {
+
+        System.out.println("Roman Calculator");
+        System.out.println("Enter calculate: ");
+
+    }
+}
