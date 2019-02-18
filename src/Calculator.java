06d6f7d761da582ae6758a8990b7e83cea445014
kihors <kihors@gmail.com>
2019-02-18 03:09:01 +0200

    Created class Calculator.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
 
+        ArabicCalc arabicCalc = new ArabicCalc();
+        RomanCalc romanCalc = new RomanCalc();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("My simple Calculator");
+
+        while (true) {
+
+            System.out.println("Please, make your choice!");
+            System.out.println("Enter 1 -> Arabic Calculator");
+            System.out.println("Enter 2 -> Roman Calculator");
+            System.out.println("Enter 0 -> exit");
+
+            int choiceCalc = scanner.nextInt();
+
+            switch (choiceCalc) {
+                case 1:
+                    arabicCalc.calculator();
+                    break;
+                case 2:
+                    romanCalc.calculator();
+                    break;
+                case 0:
+                    System.out.println("Exit");
+                    break;
+            }
+
+            if (choiceCalc == 0)
+                break;
+        }
     }
 }
